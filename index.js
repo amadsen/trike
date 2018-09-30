3d74e9d3ce3245f544a00e5bdf76fc944f22b058
Aaron Madsen <amadsen@codifiant.com>
2018-09-30 01:32:58 -0600

    Sigificantly simplify async/await support

--- index.js
+++ index.js
@@ -1,29 +1,11 @@
-
-// If we can initialize asyncTrike using eval
-// then we have async function support.
-// NOTE: we are explicitly using var here so
-// that our call to trike does not have issues
-// with hoisted functions and the temporal dead
-// zone causing trike's check for asyncTrike to
-// throw a reference error.
-var [e, asyncTrike] = trike(() => eval(`(() => {
-  const asyncTrike = async (p) => {
-    try {
-      let r = await Promise.resolve(p);
-      return [null, r];
-    } catch(e) {
-      return [e];
-    }
-  }
-  return asyncTrike;
-})()`));
-
 function trike(fn, ...args) {
   try {
     const r = fn(...args);
-    // if r is Promise-like, await it
-    if (asyncTrike && r.then && r.catch) {
-      return asyncTrike(r);
+    // if r is Promise-like, resolve it
+    if (r.then && r.catch) {
+      return r
+        .then((v) => [null, v])
+        .catch((e) => [e]);
     }
     return [null, r];
   } catch(e) {
--- test/_async-success.js
+++ test/_async-success.js
@@ -1,29 +1,9 @@
 const test = require('tape');
 
 const trike = require('../');
-test(
-  'Should return a Promise when passed a function that returns a Promise and not awaited',
-  (t) => {
-    const aSuccess = ['Success'];
-    const result = trike(
-      async (r) => r,
-      aSuccess
-    );
-    t.ok(!Array.isArray(result), 'result is not an array');
-    t.ok(typeof result.then === 'function' && typeof result.catch === 'function', 'result has then() & catch() methods');
-    result.then((r) => setImmediate(() => {
-      t.ok(Array.isArray(r), 'result resolves to an array');
-      t.ok(r.length === 2, 'resolved array has two elements');
-      t.ok(r[0] === null, 'resolved array has null in the error position');
-      t.equals(r[1], aSuccess, 'resolved array has the expected result');
-
-      t.end();
-    }));
-  }
-);
 
 test(
-  'Should catch an error from a rejecting Promise and return it as the only argument in an array',
+  'Should return a callback-like array to await',
   (t) => (async () => {
     const aSuccess = ['Success', '!'];
     const expected = 'Success!';
