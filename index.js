ad479f7b8984e55508f336b26e22752aeb88c85c
Aaron Madsen <amadsen@codifiant.com>
2018-12-07 22:30:32 -0700

    Be safer when detecting a Promise

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function trike(fn, ...args) {
   try {
     var r = fn(...args);
     // if r is Promise-like, resolve it
-    if (r.then && r.catch) {
+    if (r && r.then && r.catch) {
       return r
         .then((v) => [null, v])
         .catch((e) => [e]);
